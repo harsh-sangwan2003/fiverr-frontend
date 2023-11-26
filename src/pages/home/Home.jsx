f1366bd8623196b542364266f118dd0aaa8d1860
Harsh Sangwan <84121947+harsh-sangwan2003@users.noreply.github.com>
2023-11-26 11:15:52 +0530

    added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,10 +6,62 @@ import Slide from '../../components/slide/Slide'
 
 const Home = () => {
   return (
-    <div>
+    <div className='home'>
       <Featured />
       <TrustedBy />
       <Slide />
+      <div className="features">
+
+        <div className="container">
+
+          <div className="item">
+
+            <h1>A whole world of freelance talent at your fingertips</h1>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              The best for every budget
+            </div>
+            <p>
+              Find hight-quality services at every price point. No hourly rates,
+              just project-based pricing.
+            </p>
+
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              Qulaity work done quickly
+            </div>
+            <p>
+              Find the right freelancer to begin working on your
+              project within minutes.
+            </p>
+
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              Protected payments, every time
+            </div>
+            <p>
+              Always know what you'll pay upfront. Your payment
+              isn't released until you approve the work.
+            </p>
+
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              24/7 support
+            </div>
+            <p>
+              Find hight-quality services at every price point. No hourly rates,
+              just project-based pricing.
+            </p>
+
+          </div>
+
+          <div className="item">
+            <video src="./img/video.mp4" controls></video>
+          </div>
+
+        </div>
+
+      </div>
     </div>
   )
 }
